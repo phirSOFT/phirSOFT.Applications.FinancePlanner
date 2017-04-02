47d90271499a40bc4dc427d0cf86ddf3f893662f
Philemon Eichin <phileicht@gmx.de>
2017-04-02 20:46:04 +0200

    Setup Shell

---
+++ FinancePlanner/Shared/RegionNames.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phirSOFT.Applications.FinancePlanner
+{
+
+    /// <summary>
+    /// Contains the names of all known regions
+    /// </summary>
+    public static class RegionNames
+    {
+        public const string ShellApplicationMenu = "SHELL_RIBBON_APPLICATIONMEN";
+        public const string ShellStatusBar = "SHELL_RIBBON_STATUSBAR";
+    }
+}
