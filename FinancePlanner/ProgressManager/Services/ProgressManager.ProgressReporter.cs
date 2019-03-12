d5a409413934a63f439b12cd5ddfbeb7d4143ca7
Philemon Eichin <phileicht@gmx.de>
2019-03-12 21:40:08 +0100

    Prepare merge

---
+++ FinancePlanner/ProgressManager/Services/ProgressManager.ProgressReporter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace phirSOFT.Applications.FinancePlanner.ProgressManager.Services
+{
+    internal partial class ProgressManager : IProgressManager
+    {
+        /// <summary>
+        /// Provides a custom implementation of the <see cref="IProgress{T}"/> interface, that assures, that message is preserved.
+        /// </summary>
+        private sealed class ProgressReporter : IProgress<ProgressReport>, IDisposable
+        {
+            private readonly BlockingCollection<ProgressReport> _messages;
+
+            public ProgressReporter()
+            {
+                _messages = new BlockingCollection<ProgressReport>();
+            }
+
+            /// <inheritdoc />
+            public void Report(ProgressReport value)
+            {
+                _messages.Add(value);
+            }
+
+            /// <inheritdoc />
+            public void Dispose()
+            {
+                _messages?.Dispose();
+            }
+        }
+    }
+
+ 
+}
